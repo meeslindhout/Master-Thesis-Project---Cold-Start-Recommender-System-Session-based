5dd792948708aa78f827a76db552b264f9acaf06
meeslindhout <meeslindhout@outlook.com>
2024-06-10 12:48:50 +0200

    added rew_ward dict to wandb logging

--- src/recsys_rl.py
+++ src/recsys_rl.py
@@ -452,9 +452,10 @@ class rl_recommender():
                     "gamma": self.agent.gamma,
                     "architecture": "DQN",
                     "dataset_name": self.dataset_name,
-                    "version": "0.1",
+                    "version": "1.0",
                     "epochs": self.num_episodes,
                     "n_history": self.n_history,
+                    "reward_dict": str(self.reward_dict)
                     "memory": self.memory,
                     "batch_size": self.batch_size,
                     "target_update_freq": self.target_update_freq,
